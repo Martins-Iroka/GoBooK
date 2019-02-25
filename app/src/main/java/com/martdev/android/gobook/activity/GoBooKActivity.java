c9b32150ec53f761131cd9a19245fa05b0531c8c
Martins-Iroka <martins.iroka@gmail.com>
2019-02-25 12:40:49 +0100

    Improved the app's functions

--- app/src/main/java/com/martdev/android/gobook/activity/GoBooKActivity.java
+++ app/src/main/java/com/martdev/android/gobook/activity/GoBooKActivity.java
@@ -10,11 +10,12 @@ import android.net.NetworkInfo;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -33,8 +34,6 @@ public class GoBooKActivity extends AppCompatActivity implements LoaderCallbacks
 
     private static final int GOBOOK_LOADER_ID = 1;
 
-    private EditText mKeywordSearch;
-    private Button mSearchButton;
     private ListView mBookList;
     private TextView mEmptyTextView;
     private ProgressBar mProgressBar;
@@ -47,9 +46,8 @@ public class GoBooKActivity extends AppCompatActivity implements LoaderCallbacks
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_go_book);
 
-        mKeywordSearch = findViewById(R.id.keyword);
-
         mEmptyTextView = findViewById(R.id.empty_view);
+        mEmptyTextView.setText(R.string.perform_search);
 
         mProgressBar = findViewById(R.id.loading_indicator);
         mProgressBar.setVisibility(View.GONE);
@@ -70,56 +68,82 @@ public class GoBooKActivity extends AppCompatActivity implements LoaderCallbacks
                 startActivity(intent);
             }
         });
-
-        mSearchButton = findViewById(R.id.search_button);
-        checkNetwork();
-
     }
 
-    private void checkNetwork() {
-        mSearchButton.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.keyword_search, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.menu_item_search);
+        final SearchView searchView = (SearchView) searchItem.getActionView();
+
+        query(searchView);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void query(SearchView searchView) {
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
-            public void onClick(View v) {
-                mKeyword = mKeywordSearch.getText().toString().trim();
-                mURL =  getUrl(mKeyword);
-
-                // Get a reference to the ConnectivityManager to check state of network connectivity
-                ConnectivityManager connMgr = (ConnectivityManager)
-                        getSystemService(Context.CONNECTIVITY_SERVICE);
-
-                // Get details on the currently active default data network
-                NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-
-                // If there is a network connection, fetch data
-                if (networkInfo != null && networkInfo.isConnected()) {
-                    // Get a reference to the LoaderManager, in order to interact with loaders.
-                    LoaderManager loaderManager = getLoaderManager();
-                    mProgressBar.setVisibility(View.VISIBLE);
-
-                    if (loaderManager.getLoader(GOBOOK_LOADER_ID) == null) {
-                        // Initialize the loader. Pass in the int ID constant defined above and pass in null for
-                        // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
-                        // because this activity implements the LoaderCallbacks interface).
-                        loaderManager.initLoader(GOBOOK_LOADER_ID, null, GoBooKActivity.this);
-                        Log.i("MainActivity", "initLoader called");
-                    } else {
-                        loaderManager.restartLoader(GOBOOK_LOADER_ID, null, GoBooKActivity.this);
-                        Log.i("MainActivity", "restartLoader called");
-                    }
-                } else {
-                    // Otherwise, display error
-                    // First, hide loading indicator so error message will be visible
-                    mProgressBar.setVisibility(View.GONE);
-
-                    // Update empty state with no connection error message
-                    mEmptyTextView.setText(R.string.no_internet_connection);
+            public boolean onQueryTextSubmit(String query) {
+                if (!(query == null)){
+                    Log.i("GoBookActivity", "QueryTextSubmit: " + query);
+                    checkNetwork(query);
+                    return true;
                 }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
             }
         });
     }
 
+    private void checkNetwork(String keyword) {
+
+        mKeyword = keyword;
+        mURL = getUrl(mKeyword);
+
+        // Get a reference to the ConnectivityManager to check state of network connectivity
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // Get details on the currently active default data network
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        // If there is a network connection, fetch data
+        if (networkInfo != null && networkInfo.isConnected()) {
+            // Get a reference to the LoaderManager, in order to interact with loaders.
+            LoaderManager loaderManager = getLoaderManager();
+            mEmptyTextView.setVisibility(View.GONE);
+            mProgressBar.setVisibility(View.VISIBLE);
+
+            if (loaderManager.getLoader(GOBOOK_LOADER_ID) == null) {
+                // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+                // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+                // because this activity implements the LoaderCallbacks interface).
+                loaderManager.initLoader(GOBOOK_LOADER_ID, null, GoBooKActivity.this);
+                Log.i("MainActivity", "initLoader called");
+            } else {
+                mAdapter.clear();
+                loaderManager.restartLoader(GOBOOK_LOADER_ID, null, GoBooKActivity.this);
+                Log.i("MainActivity", "restartLoader called");
+            }
+        } else {
+            // Otherwise, display error
+            // First, hide loading indicator so error message will be visible
+            mProgressBar.setVisibility(View.GONE);
+
+            // Update empty state with no connection error message
+            mEmptyTextView.setText(R.string.no_internet_connection);
+        }
+
+    }
+
     private String getUrl(String keyword) {
-        return "https://www.googleapis.com/books/v1/volumes?q="+keyword+
+        return "https://www.googleapis.com/books/v1/volumes?q=" + keyword +
                 "&maxResults=20&AIzaSyCYgkoE5Dr07aRi4XGtv6W1ypOd3A8aci0";
     }
 
